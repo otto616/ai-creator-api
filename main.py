1cdcbf8fde30e92ffeef94ae2a77707659962a5c
otto616 <ottobielrodenas@gmail.com>
2026-05-15 12:57:42 +0200

    Fix slowapi request type and adjust CORS configuration

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
     CORSMiddleware, #ignore type
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
